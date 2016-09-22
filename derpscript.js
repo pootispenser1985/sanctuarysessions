c64807b7c81a1bd529108499a7bb0bdc30484321
pootispenser1985 <jigostowski@gmail.com>
2016-09-22 00:37:07 -0500

    can't get the clicker clickin

--- derpscript.js
+++ derpscript.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   console.log("test one two");
 
-  document.getElementById("morevideos").onclick = function() {
-    $();
-  };
+  var theElement = document.getElementById("morevideos");
+
+  theElement.bind("click", function() {
+    $("p").hide();
+  });
+
 });
 
 //this is just some placeholder stuff to figure out how to do an onclick event

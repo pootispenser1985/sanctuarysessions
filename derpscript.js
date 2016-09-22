1578426176328d89952487343cf3cc3d87e39a4b
pootispenser1985 <jigostowski@gmail.com>
2016-09-21 22:27:59 -0500

    added some vids, need to get the scrolling thingy to work

---
+++ derpscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  console.log("test one two");
+
+  document.getElementById("morevideos").onclick = function() {
+    $();
+  };
+});
+
+//this is just some placeholder stuff to figure out how to do an onclick event

6eeebe9699431859e7cf68386d306963630a8cd9
pootispenser1985 <jigostowski@gmail.com>
2016-09-27 13:57:30 -0500

    the click event works now, need to hide/show divs

--- derpscript.js
+++ derpscript.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
-  console.log("test one two");
-
-  var theElement = document.getElementById("morevideos");
-
-  theElement.bind("click", function() {
-    $("p").hide();
-  });
-
+  $('#morevideos').bind('click', alerter)
 });
 
+alerter = function() { 
+    alert("whawejrtewr");
+}
 //this is just some placeholder stuff to figure out how to do an onclick event

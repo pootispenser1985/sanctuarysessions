896b600f6e196a490335b2939a8fc38eb073066e
pootispenser1985 <jigostowski@gmail.com>
2016-09-27 21:49:11 -0500

    the video scrolling doodlie now works as intended

--- derpscript.js
+++ derpscript.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-    window.vidrows = [$('#vidrow0'), $('#vidrow1')];
+    window.vidrows = [$('#vidrow0'), $('#vidrow1'), $('#vidrow2')];
     window.shownrow = 0;
 
     vidrows[1].hide();
+    vidrows[2].hide();
     $('#morevideos').bind('click', shownextrow);
 });
 
 shownextrow = function() {
-    vidrows[shownrow].hide();
-    
-    shownrow = shownrow + 1; 
-    vidrows[shownrow].show();
+  /* if already on the last element, do this: */
+    if ((vidrows.length - 1) === shownrow) {
+      vidrows[shownrow].hide();
+      window.shownrow = 0;
+      vidrows[shownrow].show();
+    }
+  /*if you're anywhere else in the list, do this: */
+    else {
+      vidrows[shownrow].hide();
+      shownrow = shownrow + 1;
+      vidrows[shownrow].show();
+  }
 }

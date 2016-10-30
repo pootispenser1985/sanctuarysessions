cf008a9368f84f3979cea93f9f59a15d330de62c
pootispenser1985 <jigostowski@gmail.com>
2016-10-29 19:35:21 -0500

    got the functionality of the mobile popup nav bar going

--- derpscript.js
+++ derpscript.js
@@ -4,20 +4,36 @@ $(document).ready(function() {
 
     vidrows[1].hide();
     vidrows[2].hide();
+
     $('#morevideos').bind('click', shownextrow);
+    $('#toggle-menu').bind('click', togglemenu);
 });
 
 shownextrow = function() {
   /* if already on the last element, do this: */
-    if ((vidrows.length - 1) === shownrow) {
-      vidrows[shownrow].hide();
-      window.shownrow = 0;
-      vidrows[shownrow].show();
-    }
-  /*if you're anywhere else in the list, do this: */
-    else {
-      vidrows[shownrow].hide();
-      shownrow = shownrow + 1;
-      vidrows[shownrow].show();
+  if ((vidrows.length - 1) === shownrow) {
+    vidrows[shownrow].hide();
+    window.shownrow = 0;
+    vidrows[shownrow].show();
   }
+  /*if you're anywhere else in the list, do this: */
+  else {
+    vidrows[shownrow].hide();
+    shownrow = shownrow + 1;
+    vidrows[shownrow].show();
+  }
+}
+
+i = 0;
+togglemenu = function() {
+  if (i % 2 === 0) {
+    $(".navitem").removeClass("hidden-xs");
+    $(".navitem").removeClass("hidden-sm");
+    $(".navitem").addClass("text-center");
+  }
+  else {
+    $(".navitem").addClass("hidden-xs");
+    $(".navitem").addClass("hidden-sm");
+  }
+  i += 1;
 }
